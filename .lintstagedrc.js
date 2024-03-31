cba5373a56decc89ca6eb7f065003681e745bc52
guilhermeabell <gpeuvis@gmail.com>
2024-03-31 15:40:54 -0300

    chore: adding jest-styled-components and fix husky

--- .lintstagedrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  // Lint and prettify TS and JS (SRC) files
-  '*.{js,jsx,ts,tsx}': (filenames) => [
-    `prettier --write ${filenames.join(' ')}`,
-    `npm run lint --fix ${filenames.join(' --file')}`,
-    `npm test -- --findRelatedTests ${filenames.join(' ')}`
-  ]
-}
--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,9 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/app/**', '!src/lib/registry.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/'],
-  transform: { '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }] }
+  transform: { '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }] },
+  moduleNameMapper: {
+    '^styled-components':
+      '<rootDir>/node_modules/styled-components/dist/styled-components.browser.cjs.js'
+  }
 }
